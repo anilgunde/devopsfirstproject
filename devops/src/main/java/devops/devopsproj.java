dc7478c9bddeb9a08d58134f324c264b03a6a963
ANIL <ANIL@DESKTOP-EMT4JQD>
2022-06-18 21:54:33 +0530

    second commit

---
+++ devops/src/main/java/devops/devopsproj.java
@@ -0,0 +1,10 @@
+package devops;
+
+public class devopsproj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world this is my first project");
+	}
+
+}
